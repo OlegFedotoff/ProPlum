5cfb70303841edcaffd2da85870594da8eb7e5c6
dvs65 <dvs65@komus.net>
2025-11-12 17:34:48 +0300

    DWH-39885 Изменение групп загрузки объектов с ADWH на ECOM

---
+++ fw/releases/000022/change.sql
@@ -0,0 +1,2 @@
+update fw.objects set load_group = 'ECOM' where load_group = 'ADWH';
+update fw.objects set load_group = 'ECOM_WITHOUT_KDW' where load_group = 'ADWH_WITHOUT_KDW';
---
+++ fw/releases/000022/rollback.sql
@@ -0,0 +1,2 @@
+update fw.objects set load_group = 'ADWH' where load_group = 'ECOM';
+update fw.objects set load_group = 'ADWH_WITHOUT_KDW' where load_group = 'ECOM_WITHOUT_KDW';
