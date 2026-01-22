5636ce32c184ab0748cb31207bd88f72fd342416
dvs65 <dvs65@komus.net>
2026-01-22 11:18:50 +0300

    DWH-44308 Перенос объектов analyze таблиц в отдельную. В этом релизе убираю зависимости, которые теперь не нужны

---
+++ fw/releases/000030/remove_dependencies.sql
@@ -0,0 +1,4 @@
+delete from fw.dependencies 
+where object_id = 775 and object_id_depend = 698;
+delete from fw.dependencies 
+where object_id = 776 and object_id_depend = 756;
---
+++ fw/releases/000030/rollback_dependencies.sql
@@ -0,0 +1,8 @@
+-- Восстановление зависимостей
+INSERT INTO fw.dependencies
+(object_id, object_id_depend)
+VALUES(775, 698);
+
+INSERT INTO fw.dependencies
+(object_id, object_id_depend)
+VALUES(776, 756);
