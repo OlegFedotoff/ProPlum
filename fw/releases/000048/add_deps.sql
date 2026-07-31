64e0ec3e360898cb3bdbad1a3077ba9fffb4bc6b
dvs65 <dvs65@komus.net>
2026-07-31 14:28:19 +0300

    DWH-50871

---
+++ fw/releases/000048/add_deps.sql
@@ -0,0 +1,7 @@
+delete from fw.dependencies where object_id = 741;
+INSERT INTO fw.dependencies
+(object_id, object_id_depend)
+VALUES(741, 691);
+INSERT INTO fw.dependencies
+(object_id, object_id_depend)
+VALUES(741, 698);
---
+++ fw/releases/000048/rollback_deps.sql
@@ -0,0 +1,10 @@
+delete from fw.dependencies where object_id = 741;
+INSERT INTO fw.dependencies
+(object_id, object_id_depend)
+VALUES(741, 524);
+INSERT INTO fw.dependencies
+(object_id, object_id_depend)
+VALUES(741, 529);
+INSERT INTO fw.dependencies
+(object_id, object_id_depend)
+VALUES(741, 578);
